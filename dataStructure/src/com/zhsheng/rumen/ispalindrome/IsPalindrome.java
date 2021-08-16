8dd4cd03b0ce3236e7798b245044b2edf948a1ca
ZhangSheng0540 <zhangsheng0540@163.com>
2021-08-16 22:12:57 +0800

    回文字符串  在字符串上直接判断

--- dataStructure/src/com/zhsheng/rumen/ispalindrome/IsPalindrome.java
+++ dataStructure/src/com/zhsheng/rumen/ispalindrome/IsPalindrome.java
@@ -10,6 +10,29 @@ package com.zhsheng.rumen.ispalindrome;
 public class IsPalindrome {
 
     public static boolean isPalindrome(String s) {
+
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (i < j ) {
+                if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+        return true;
+
+    }
+
+    public static boolean isPalindrome1(String s) {
         // 将字符串 去掉空格和特殊字符 全部转换为小写
         String lowerCase = s.toLowerCase();
         char[] chars = lowerCase.toCharArray();
@@ -33,7 +56,7 @@ public class IsPalindrome {
     }
 
     public static void main(String[] args) {
-        String s = "race a car";
+        String s = "A man, a plan, a canal: Panama";
         System.out.println(IsPalindrome.isPalindrome(s));
     }
 }
